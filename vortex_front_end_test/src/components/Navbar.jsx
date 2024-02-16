62e619ac64756cbc0a4b51a8f71df9db4c1a8af7
kiqsmg <kiqsmg@gmail.com>
2024-02-15 23:59:04 -0300

    Add changes to the navbar composition

--- vortex_front_end_test/src/components/Navbar.jsx
+++ vortex_front_end_test/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
+
+import { close, logo, menu } from '../assets';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      
+    </nav>
   )
 }
 
