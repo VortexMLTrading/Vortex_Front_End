54efbc412177a71306e3927ba753d82250b608c3
kiqsmg <kiqsmg@gmail.com>
2024-03-04 10:01:16 -0300

    Fix the issues with the width lenght

--- vortex_front_end_test/src/components/Hero.jsx
+++ vortex_front_end_test/src/components/Hero.jsx
@@ -8,18 +8,18 @@ const Hero = () => (
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
 
       <div className="flex flex-row justify-between items-center w-full">
-        <h1 className="flex-1 font-poppins font-semibold ss:text-[52px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[50px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
           Transformando <span className="text-gradient">dados</span> <br className="sm:block hidden"/>
           em <span className="text-gradient">oportunidades</span>
         </h1>
       </div>
 
-      <p className={`${styles.paragraph} max-w-[520px] mt-5`}>
-        Bem vindo ao  <span className="text-gradient text-[22px]">VORTEX!</span> A sua plataforma de Algoritmos <br/>de Tranding. 
-        Transforme a sua vida financeira através do<br/> nosso BOT impulsionado por Machine Learning.
+      <p className={`${styles.paragraph} max-w-[620px] mt-5 ss:text-[24px]`}>
+        Bem vindo ao  <span className="text-gradient text-[30px]">VORTEX!</span> A sua plataforma de Algoritmos de Tranding. 
+        Transforme a sua vida financeira através do nosso BOT impulsionado por Machine Learning.
       </p>
 
-      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[20px] mb-2 mt-5">
+      <div className="flex flex-row items-center py-[6px] px-6 bg-blue-gradient text-black font-bold rounded-[20px] mb-5 mt-5 ss:text-[32px]">
         <p>Conheça os BOTs</p>
       </div>
 
@@ -27,7 +27,7 @@ const Hero = () => (
 
 
     <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+        <img src={robot} alt="billing" className="w-[80%] h-[80%] relative z-[5]" />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
