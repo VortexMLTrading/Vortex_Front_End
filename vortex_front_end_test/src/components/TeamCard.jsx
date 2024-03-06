7c5eda10ec42a8cb3bb7ee83fb985283d25f6908
kiqsmg <kiqsmg@gmail.com>
2024-03-05 23:53:24 -0300

    Fix the center button and text problem inside the teamplayer card

--- vortex_front_end_test/src/components/TeamCard.jsx
+++ vortex_front_end_test/src/components/TeamCard.jsx
@@ -1,18 +1,19 @@
 
 const TeamCard = ({ name, title, img }) => (
 
-  <div className="flex flex-col items-center text-center pb-10 bg-white mr-10 w-[16rem] h-[20rem] max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <img src={img} alt={name} className="w-[8rem] h-[8rem] rounded-full w-24 h-24  mt-6 mb-6 rounded-full shadow-lg" />
-    <div className="flex flex-col ml-4">
-      <h4 className="font-poppins font-semibold text-gray-900 text-[20px] leading-[32px]">
-        {name}
-      </h4>
-      <p className="font-poppins font-normal text-[16px] leading-[24px] text-gray-400 mt-2 mb-4">
-        {title}
-      </p>
-      <a href="#" class=" inline-flex py-2 text-sm text-center w-[8rem] font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Know more</a>
-    </div>
+  <div className="flex flex-col items-center justify-center pb-10 bg-white mr-10 w-[16rem] h-[20rem] max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <img src={img} alt={name} className="w-[8rem] h-[8rem] rounded-full w-24 h-24 mt-6 mb-6 rounded-full shadow-lg" />
+  <div className="flex flex-col ml-4 items-center">
+    <h4 className="font-poppins font-semibold text-gray-900 text-[20px] leading-[32px]">
+      {name}
+    </h4>
+    <p className="font-poppins font-normal text-[16px] leading-[24px] text-gray-400 mt-2 mb-4">
+      {title}
+    </p>
+    <a href="#" className="inline-flex justify-center items-center py-2 text-sm w-[8rem] font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Know more</a>
   </div>
+</div>
+
 )
 
 export default TeamCard;
