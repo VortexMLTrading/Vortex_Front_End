e500d417f2b99a1b35f8d74fd71b2124c78f27f0
kiqsmg <kiqsmg@gmail.com>
2024-03-07 14:53:24 -0300

    Refactor the components folder, delete unecessary files for the main page

--- vortex_front_end_test/src/components/index.js
+++ vortex_front_end_test/src/components/index.js
@@ -1,26 +1,18 @@
 import Navbar from "./Navbar";
-import Billing from "./Billing";
-import CardDeal from "./CardDeal";
 import Team from "./Team";
-import Clients from "./Clients";
 import CTA from "./CTA";
 import Stats from "./Stats";
 import Footer from "./Footer";
-import Testimonials from "./Testimonials";
 import Hero from "./Hero";
 import About from "./About";
 import Business from "./Business";
 
 export {
   Navbar,
-  Billing,
-  CardDeal,
   Team,
-  Clients,
   CTA,
   Stats,
   Footer,
-  Testimonials,
   Hero,
   About,
   Business,
