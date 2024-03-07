63fff8112104089f0fdafdf79a34d071b14367c8
kiqsmg <kiqsmg@gmail.com>
2024-03-07 14:52:09 -0300

    Refactor the assets folder, delete unecessary files for the main page

--- vortex_front_end_test/src/assets/index.js
+++ vortex_front_end_test/src/assets/index.js
@@ -1,9 +1,3 @@
-import airbnb from "./airbnb.png";
-import bill from "./bill.png";
-import binance from "./binance.png";
-import card from "./card.png";
-import coinbase from "./coinbase.png";
-import dropbox from "./dropbox.png";
 import logo from "./logo.svg";
 import logovortex from "./logovortex.svg";
 import quotes from "./quotes.svg";
@@ -13,14 +7,8 @@ import shield from "./Shield.svg";
 import star from "./Star.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import google from "./google.svg";
-import apple from "./apple.svg";
 import arrowUp from "./arrow-up.svg";
-import discount from "./Discount.svg";
 import instagram from "./instagram.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
 import logo_grande from "./logo_grande.svg";
 import teamplayer01 from "./teamplayer01.jpeg";
 import teamplayer02 from "./teamplayer02.jpeg";
@@ -32,12 +20,6 @@ import tiktok from "./tiktok.svg";
 
 export {
   logo_grande,
-  airbnb,
-  bill,
-  binance,
-  card,
-  coinbase,
-  dropbox,
   logo,
   logovortex,
   quotes,
@@ -47,14 +29,8 @@ export {
   star,
   menu,
   close,
-  google,
-  apple,
   arrowUp,
-  discount,
   instagram,
-  people01,
-  people02,
-  people03,
   whatsapp,
   tiktok,
   teamplayer01,
